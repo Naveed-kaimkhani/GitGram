9c24c0bf48d390c302f6d1b1c8d12f259a463f84
Naveed kaimkhani <naveedkaimkhami@gmail.com>
2022-03-08 14:47:08 +0500

    .

--- lib/ui/Screens/repoview.dart
+++ lib/ui/Screens/repoview.dart
@@ -27,51 +27,46 @@ class _repoviewState extends State<repoview> {
           elevation: 0,
         ),
         body: Column(
+          
           children: [
+        
             Expanded(
-              child: ListView.builder(
-                itemCount: reposInfo.length,
-                itemBuilder: (context,index){
-                  return ListTile(
-                  
-                  );
-                }
-
-
-              // child: GridView.count(
-              //   crossAxisCount: 1,
-              //   crossAxisSpacing: 5,
-              //   mainAxisSpacing: 5,
-              //   children: reposInfo
-              //       .map((item) => GestureDetector(
-              //             onTap: () {
-              //               Navigator.of(context).pushNamed(AppRoutes.Repo_webview,
-              //                   arguments: item!.html_url);
-              //             },
-              //             child: Container(
-              //               decoration: BoxDecoration(
-              //                 border: Border.all(
-              //                   color: Colors.grey,
-              //                 ),
-              //                 borderRadius: BorderRadius.all(Radius.circular(12)),
-              //               ),
-              //               child: Card(
-              //                 color: Colors.black,
-              //                 child: Center(
-              //                   child: Text(
-              //                     item?.name ?? "Repo name is null",
-              //                     style: TextStyle(color: Colors.white),
-              //                     maxLines: 1,
-              //                     overflow: TextOverflow.ellipsis,
-              //                   ),
-              //                 ),
-              //               ),
-              //             ),
-              //           ))
-              //       .toList(),
-              // ),
+              child: GridView.count(
+                crossAxisCount: 1,
+                crossAxisSpacing: 5,
+                mainAxisSpacing: 5,
+                children: reposInfo
+                    .map((item) => GestureDetector(
+                          onTap: () {
+                            Navigator.of(context).pushNamed(AppRoutes.Repo_webview,
+                                arguments: item!.html_url);
+                          },
+                          child: Container(
+                            height: 2,
+                            decoration: BoxDecoration(
+                              border: Border.all(
+                                color: Colors.grey,
+                              ),
+                              borderRadius: BorderRadius.all(Radius.circular(12)),
+                            ),
+                            child: Card(
+                            
+                              color: Colors.black,
+                              child: Center(
+                                child: Text(
+                                  item?.name ?? "Repo name is null",
+                                  style: TextStyle(color: Colors.white),
+                                  maxLines: 1,
+                                  overflow: TextOverflow.ellipsis,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ))
+                    .toList(),
+              ),
             )
-            )
+            
           ]
         ));
   }
